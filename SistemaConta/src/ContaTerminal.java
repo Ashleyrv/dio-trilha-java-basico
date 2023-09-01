f6598663ee018fa3e7cce126018813370500b737
Ashley <ashleyvianar15@gmail.com>
2023-08-31 22:39:26 -0300

    initial commit

---
+++ SistemaConta/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Por favor, digite seu nome: ");
+        String nomeCliente = scan.nextLine();
+
+        System.out.print("Número da conta: ");
+        int numConta = scan.nextInt();
+
+        System.out.print("Agência: ");
+        String agencia = scan.next();
+
+
+        System.out.print("Saldo da conta: ");
+        double saldo = scan.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, " +
+                "conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, agencia, numConta, saldo);
+
+        scan.close();
+    }
+}
+
